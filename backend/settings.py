51677998274e0097a7a1fbb464a857abb02cee92
f.celletti <fabrus98@gmail.com>
2025-10-17 15:07:47 +0200

    .

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 BACKEND_URL = "http://0.0.0.0:8000"
 
 # URL frontend
-FRONTEND_URL = "http://localhost:5173"
+FRONTEND_URL = "http://87.106.39.97:5173"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
